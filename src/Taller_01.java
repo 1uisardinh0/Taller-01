d7a1e42d2cda817904a8984ac99f0d79c15d46f4
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-08-29 09:11:21 -0400

    Se craron los métodos main y menu del programa

---
+++ src/Taller_01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Taller_01 {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    //Muestra las opciones del menu del programa.
+    public static void menu(){
+
+        System.out.println("""
+                ---Bienvenido al menu del programa---
+                
+                elija una opcion:
+                    1. Verificar Revés-Derecho.
+                    2. Contar vocales de una frase.
+                    3. Encriptar una frase.
+                    4. Desencriptar una frase.
+                    5. Salir.""");
+    }
+
+
+}
