6c9a247161c08edd16154a210b4041543dee6ea5
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-08-29 10:01:34 -0400

    Se intentó mejorar el menu del programa. Se agregaron los metodos para verificar si la frase es revéz-derecho, contar vocales, encriptar una frase, desencriptar una frase y salir.

--- src/Taller_01.java
+++ src/Taller_01.java
@@ -7,15 +7,61 @@ public class Taller_01 {
 
     //Muestra las opciones del menu del programa.
     public static void menu(){
+        mostrarMenu();
+    }
 
-        System.out.println("""
-                ---Bienvenido al menu del programa---
-                
-                Elija una opcion:
-                    1. Verificar Revés-Derecho.
-                    2. Contar vocales de una frase.
-                    3. Encriptar una frase.
-                    4. Desencriptar una frase.
-                    5. Salir.""");
+    public static void mostrarMenu(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        while (true){
+            System.out.println("""
+                    ---Bienvenido al menu del programa---
+                    
+                        1. Verificar Revés-Derecho.
+                        2. Contar vocales de una frase.
+                        3. Encriptar una frase.
+                        4. Desencriptar una frase.
+                        5. Salir.
+                        
+                    Elija una opcion: """);
+
+            String opcion =  entrada.nextLine();
+
+            switch(opcion){
+                case "1": verificarRevezDerecho();
+                    break;
+
+                case "2": contarVocales();
+                    break;
+
+                case "3": encriptarFrase();
+                    break;
+
+                case "4": desencriptarFrase();
+                    break;
+
+                case "5": break;
+
+            }
+
+        }
+    }
+
+    public static void verificarRevezDerecho(){
+        System.out.println("Hola esto funciona");
+    }
+
+    public static void contarVocales(){
+        System.out.println("Hola esto funciona");
+    }
+
+    public static void encriptarFrase(){
+        System.out.println("Hola esto funciona");
+    }
+
+    public static void desencriptarFrase(){
+        System.out.println("Hola esto funciona");
+
     }
 }
